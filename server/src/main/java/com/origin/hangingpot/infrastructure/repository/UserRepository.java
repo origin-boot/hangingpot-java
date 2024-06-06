76b98e0710ee38ba93f790f8f992a5bbcc4e2f12
hku47w <168148058+hku47w@users.noreply.github.com>
2024-06-07 06:11:33 +0800

    feat(query): add andAnyOf and orAllOf methods to ShortcutPredicateBuilder

--- server/src/main/java/com/origin/hangingpot/infrastructure/repository/UserRepository.java
+++ server/src/main/java/com/origin/hangingpot/infrastructure/repository/UserRepository.java
@@ -38,8 +38,17 @@ class AdvancedUserRepositoryImpl extends ShortcutPagingQuery implements Advanced
 	@Override
 	public Page<User> searchUsers(String keyword, int pageSize) {
 		QUser a = QUser.user;
+		// where username like '%user%'
+		// and id>0 and (id>0 and id<1000) and (id>-1 or id<1001)
+		// or id<0 or (id<-1 or id<-2) or (id<-3 and id<-4)
 		Predicate p = predicate()
 				.and(isNotBlank(keyword), a.username.like(quoteLike(keyword)))
+				.and(a.id.gt(0))
+				.and(a.id.gt(0), a.id.lt(1000))
+				.andAnyOf(true, a.id.gt(-1), a.id.lt(1001))
+				.or(a.id.lt(0))
+				.or(a.id.lt(-1), a.id.lt(-2))
+				.orAllOf(true, a.id.lt(-3), a.id.lt(-4))
 				.build();
 		Page<User> r = findAll(
 				q -> q.select(a)

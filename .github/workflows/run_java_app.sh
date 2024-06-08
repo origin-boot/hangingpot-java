16562c78b35f9728a54fbdc285180729919e6609
hku47w <168148058+hku47w@users.noreply.github.com>
2024-06-08 08:31:27 +0800

    feat(ci): modify the naming rules of ci.yml to increase support for multiple projects

--- .github/workflows/run_java_app.sh
+++ .github/workflows/run_java_app.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 
-cd server
-nohup java -jar target/hangingpot-0.0.1-SNAPSHOT.jar --spring.config.location=file:config/ci.properties > app.log 2>&1 &
+MODULE=""
+PROJECT=""
+
+while getopts ":m:p:" opt; do
+  case $opt in
+    m)
+      MODULE=$OPTARG
+      ;;
+    p)
+      PROJECT=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$MODULE" ]; then
+    echo "Error: Module name is required."
+    exit 1
+fi
+
+if [ -z "$PROJECT" ]; then
+    echo "Error: Project name is required."
+    exit 1
+fi
+
+
+cd $MODULE
+nohup java -jar target/"${PROJECT}-0.0.1-SNAPSHOT.jar" --spring.config.location=file:config/ci.properties > app.log 2>&1 &
 
 sleep 30
 
 echo "Contents of app.log:"
 cat app.log
 
-if grep -q "Started HangingpotApplication" app.log; then
+if grep -i -q "Started ${PROJECT}Application" app.log; then
     echo "Application started successfully."
     exit 0
 else
